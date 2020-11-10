62d58d3f4c1cf68ae2b94947002a81e5a2064a57
L1keor <51744307+L1keor@users.noreply.github.com>
2020-11-10 23:43:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 start = int(input('Введите номер игры с которой начнете ставить: '))
 stop = int(input('Введите номер игры на которой закончите ставить: '))
 prognoz = []
+sstavka = int(input('Введите сумму по сколько будете ставить: '))
+rmmbr = ''
+stavka = int(sstavka)
 for i in range(start, stop+1):
     if i > 25:
         n = i - 26
@@ -16,5 +19,14 @@ for i in range(start, stop+1):
         prognoz.append('Бубны')
     else: prognoz.append('Хз')
 
-for i in range(len(prognoz)):
-    print('Масть игроку в', i+start, 'игре:', prognoz[i])
+print('Масть игроку в', start, 'игре:', prognoz[1], 'Ставка:', stavka)
+
+for i in range(1, len(prognoz)):
+    chk = str(input('Заход или нет(+, -): '))
+    if chk == '+':
+        stavka = sstavka
+        print('Масть игроку в', i + start, 'игре:', prognoz[i], 'Ставка:', stavka)
+    elif chk == '-':
+        rmmbr = prognoz[i-1]
+        stavka *= 3
+        print('Масть игроку в', i + start, 'игре:', rmmbr, 'Ставка:', stavka)
